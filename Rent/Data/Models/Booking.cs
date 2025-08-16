7e1e07cdcee5ae80a3cba969ebd6c091d8d160d1
Md. Mazharul Islam <mazharul.saurav255@gmail.com>
2025-08-16 20:26:05 +0600

    change
    change

---
+++ Rent/Data/Models/Booking.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data.Models;
+
+public partial class Booking
+{
+    public int BookingId { get; set; }
+
+    public int? PropertyId { get; set; }
+
+    public int? RenterId { get; set; }
+
+    public DateOnly? CheckInDate { get; set; }
+
+    public DateOnly? CheckOutDate { get; set; }
+
+    public decimal? TotalPrice { get; set; }
+
+    public string? BookingStatus { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual ICollection<Payment> Payments { get; set; } = new List<Payment>();
+
+    public virtual Property? Property { get; set; }
+
+    public virtual User? Renter { get; set; }
+}
---
+++ Rent/Data/Models/OwnerContact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data.Models;
+
+public partial class OwnerContact
+{
+    public int ContactId { get; set; }
+
+    public int? OwnerId { get; set; }
+
+    public string? ContactType { get; set; }
+
+    public string? ContactValue { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual User? Owner { get; set; }
+}
